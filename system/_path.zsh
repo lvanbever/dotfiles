0b63f6b65957846c9c4a552f3ae7a7861368b4f0
Laurent Vanbever <lvanbever@ethz.ch>
2017-10-04 21:27:39 +0200

    last update

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,6 @@
+export PATH=/Developer/NVIDIA/CUDA-7.5/bin:$PATH
+export DYLD_LIBRARY_PATH=/Developer/NVIDIA/CUDA-7.5/lib:$DYLD_LIBRARY_PATH
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="/Users/lvanbever/Documents/workspace/gns3-legacy/src/GNS3/Dynagen:$PATH"
+export PATH="/Users/lvanbever/anaconda2/bin:$PATH"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,2 @@
 alias reload!='. ~/.zshrc'
+export correct=/Users/lvanbever/Documents/workspace/sdx_correctness_paper
